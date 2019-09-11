34056c503027df69fe68b5ff6f301d27ffcece19
ShaRose <ShawnAndrewRose@Gmail.Com>
2019-09-10 22:08:48 -0230

    Add vm-test

---
+++ hosts/vm-test/default.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }:
+# just an example top-level "configuration.nix" file within the themelios scheme
+{
+imports = [];
+
+i18n = {
+consoleFont = "Lat2-Terminus16";
+consoleKeyMap = "us";
+defaultLocale = "en_US.UTF-8";
+};
+
+time.timeZone = "America/St_Johns";
+
+programs.mtr.enable = true;
+programs.bash.enableCompletion = true;
+
+networking.hostName = "vm-test";
+}

6da57970309f8d25a33f3d9415904f8550ae5b0b
ShaRose <ShawnAndrewRose@Gmail.Com>
2020-01-11 20:20:07 -0330

    oops

--- hosts/apollo/configuration.sh
+++ hosts/apollo/configuration.sh
@@ -4,7 +4,7 @@ use_sgdisk_clear="true"     # use sgdisk --clear
 use_wipefs_all="true"       # use wipefs --all
 use_zero_disks="false"      # use dd if=/dev/zero ...
 zfs_pool_name="apollo-pool"
-zfs_pool_disks=("/dev/disk/by-id/ata-Samsung_SSD_850_PRO_256GB_S251NXAG723090T")
+zfs_pool_disks=("/dev/disk/by-id/ata-WDC_WD5001AALS-00E3A0_WD-WCATR4607600")
 zfs_pool_type=""            # use "" for single, or "mirror", "raidz1", etc.
 zfs_encrypt_home="false"    # only set to true if you are using a nixos ISO with ZFS 0.8 or higher.
 zfs_auto_snapshot=() # datasets to be set with com.sun:auto-snapshot=true

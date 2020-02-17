0a0d7494b0151f280d231c7d4093044497517e2d
ShaRose <ShawnAndrewRose@Gmail.Com>
2020-02-16 23:15:45 -0330

    Helios wifi
    Testing hostapd

--- hosts/helios/default.nix
+++ hosts/helios/default.nix
@@ -24,7 +24,40 @@ with types;
     networking.usePredictableInterfaceNames = false;
     services.udev.extraRules = ''
         KERNEL=="eth*", ATTR{address}=="b8:97:5a:96:0d:4e", NAME="gbnic"
+        KERNEL=="eth*", ATTR{address}=="74:da:38:3b:0f:d9", NAME="wlaniot"
     '';
+    
+    networking.networkmanager.unmanaged = [ "interface-name:wlaniot" ]
+    
+    services.hostapd = {
+        enable        = true;
+        interface     = "wlaniot";
+        hwMode        = "g";
+        ssid          = "RoseHome-IoT";
+        # Testing password
+        wpaPassphrase = "NcN8fz57zBLswiZ63Wum3HdwhnBaC9zrGCWNLZc8";
+        extraConfig = ''
+            ignore_broadcast_ssid=1
+        '';
+    };
+
+    networking.interfaces.wlaniot = {
+        ipv4 = {
+            addresses = [ { address = "10.70.0.1"; prefixLength = 24; } ];
+        };
+    };
+    
+    services.dnsmasq = lib.optionalAttrs config.services.hostapd.enable {
+        enable = true;
+        extraConfig = ''
+            interface=wlaniot
+            bind-interfaces
+            dhcp-range=10.70.0.10,10.70.0.254,24h
+        '';
+    };
+
+    networking.firewall.allowedUDPPorts = [53 67]; # DNS & DHCP
+
     networking.useDHCP = false;
     networking.nameservers = [ "8.8.8.8" ];
     networking.interfaces.gbnic = {
